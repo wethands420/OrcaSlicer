a1d8c13369ab76ef36dbb0c42afea94c8203e4b8
wethands420 <lucas.theisen@outlook.de>
2026-06-03 08:56:12 +0200

    Fix Windows MSVC build configuration

--- src/libslic3r/Print.cpp
+++ src/libslic3r/Print.cpp
@@ -1,3 +1,13 @@
+#ifdef WIN32
+    #ifndef WIN32_LEAN_AND_MEAN
+        #define WIN32_LEAN_AND_MEAN
+    #endif
+    #ifndef NOMINMAX
+        #define NOMINMAX
+    #endif
+    #include <windows.h>
+#endif
+
 #include "Config.hpp"
 #include "Exception.hpp"
 #include "Print.hpp"
